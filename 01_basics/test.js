77e200f8873983c08d0ec39304212504e116aedd
VishalChoudhary88 <vishalcba88@gmail.com>
2025-06-17 10:49:46 +0000

    test done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,43 @@
-console.log("vishal")
+let score = true
+
+//  console.log(typeof score);
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+//  "33"=>33
+// "33abc"=>NaN
+
+
+
+// operations
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+let srt1 = "hello"
+let str2 = " Vishal"
+
+let str3 = srt1 + str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(2+"1");
+// console.log(2+2+"1");
+// console.log("1"+2+2);
+
+// console.log(true);
+// console.log(+true);
+let nim1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter=100
+// gameCounter++;  
+ ++gameCounter;
+console.log(gameCounter);
