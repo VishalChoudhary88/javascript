f78636b903ba0e36eef518d52ad3228d971b5f28
VishalChoudhary88 <vishalcba88@gmail.com>
2025-06-18 06:28:38 +0000

    data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,32 @@
+// # primitive 
+
+//  7 types: string ,number ,boolean, null, undefined,symbol, BigInt
+
+const score = 100
+const scoreValue = 100.1
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = symbol('123')
+
+// console.log(id===anotherId);
+
+//  const bigNumber=4625456742653n
+
+// reference (non primitive):
+
+// array,objects,functions,
+const heros = ["choudhary", "kumar", "koundal"]
+let myObj = {
+    name: "vishal"
+    age: 24,
+}
+
+const myFunction = function () {
+    console.log("hello world");
+
+}
+console.log(typeof anotherId);
