72b85333e9aa579fae29bcd2b1cbe2584312afc7
VishalChoudhary88 <vishalcba88@gmail.com>
2025-06-19 05:23:51 +0000

    maths Done

--- 01_basics/05_number-maths.js
+++ 01_basics/05_number-maths.js
@@ -13,7 +13,7 @@ const balance = new Number(100)
 const ontherNumber = 1123.84569
 // console.log(ontherNumber.toPrecision(3));
 
-const hundered =100000000
+const hundered = 100000000
 // console.log(hundered.toLocaleString('en-IN'));
 
 // ************************ Maths*************************************
@@ -25,3 +25,9 @@ const hundered =100000000
 // console.log(Math.max(3,8,9,6,4));
 
 console.log(Math.random());
+console.log( Math.floor(Math.random() * 10) + 2);
+
+
+const min = 10
+const max = 20
+Math.random() * (max-min +1)
