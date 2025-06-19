310809d856490c87ca4e7e34e06ac2ca39173bc1
VishalChoudhary88 <vishalcba88@gmail.com>
2025-06-19 04:19:21 +0000

    math done

---
+++ 01_basics/05_number-maths.js
@@ -0,0 +1,27 @@
+const score = 500
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const ontherNumber = 1123.84569
+// console.log(ontherNumber.toPrecision(3));
+
+const hundered =100000000
+// console.log(hundered.toLocaleString('en-IN'));
+
+// ************************ Maths*************************************
+// console.log(Math);
+// console.log(Math.abs(5));
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(3.2));
+// console.log(Math.min(3,8,9,6,7));
+// console.log(Math.max(3,8,9,6,4));
+
+console.log(Math.random());
